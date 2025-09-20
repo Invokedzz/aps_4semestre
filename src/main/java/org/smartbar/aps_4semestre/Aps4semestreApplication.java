ff3b8c6c765ca8874155487f7733b12cf3e8b4be
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-09-20 16:26:09 -0300

    Initial commit

---
+++ src/main/java/org/smartbar/aps_4semestre/Aps4semestreApplication.java
@@ -0,0 +1,13 @@
+package org.smartbar.aps_4semestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aps4semestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aps4semestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/smartbar/aps_4semestre/Aps4semestreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smartbar.aps_4semestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aps4semestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
