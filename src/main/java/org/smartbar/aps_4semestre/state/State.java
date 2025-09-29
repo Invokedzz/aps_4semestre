29ecb6b0c3534717ecd24486dd5d400a313dcac7
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-09-29 09:37:08 -0300

    refactor: adjusting state entity

--- src/main/java/org/smartbar/aps_4semestre/state/State.java
+++ src/main/java/org/smartbar/aps_4semestre/state/State.java
@@ -1,11 +1,20 @@
 package org.smartbar.aps_4semestre.state;
 
 import org.bson.types.ObjectId;
+import org.smartbar.aps_4semestre.state.coordinate.Coordinate;
 import org.springframework.data.annotation.Id;
 
-public class State {
+public class State extends Coordinate {
 
     @Id
     private ObjectId _id;
+
+    public String pais;
+
+    public String estado;
+
+    public String municipio;
+
+    public String bioma;
 
 }
---
+++ src/main/java/org/smartbar/aps_4semestre/state/coordinate/Coordinate.java
@@ -0,0 +1,15 @@
+package org.smartbar.aps_4semestre.state.coordinate;
+
+public class Coordinate {
+
+    public int id_bdq;
+
+    public String foco_id;
+
+    public double lat;
+
+    public double lon;
+
+    public String data_pas;
+
+}
